1177f4627651e1184997ac4330eac7e772a87840
Ananya Soni <ananyasoni@Ananyas-MacBook-Pro.local>
2024-10-12 19:35:33 -0700

    added add Account function

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import numpy as np
+import sqlite3
+import json
 import pickle
 
 app = Flask(__name__)
@@ -13,8 +15,19 @@ def predict():
     data = request.get_json()
     features = np.array(data['features']).reshape(1, -1)
     prediction = model.predict(features)
-    
     return jsonify({'prediction': prediction.tolist()})
+
+def connect_db(db_name):
+    return sqlite3.connect(db_name)
+
+@app.route('/addaccount', methods=['POST'])
+def addAccount():
+    data = request.get_json()
+    login = data.username
+    password = data.password
+    conn = connect_db('User_Info.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT users (name, email) VALUES ('John Doe', 'john@example.com')")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- login.js
+++ login.js
@@ -1,27 +1,6 @@
 'use strict';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
 
 (function() {
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCe4FLr_VkD9cmN4Za6VAiGJ5GOtDAuTWQ",
-    authDomain: "tiltai.firebaseapp.com",
-    projectId: "tiltai",
-    storageBucket: "tiltai.appspot.com",
-    messagingSenderId: "818461348700",
-    appId: "1:818461348700:web:335c63db3901d1fff96799",
-    measurementId: "G-371C4M5PC5"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
 
   window.addEventListener('load', init);
 
@@ -54,8 +33,8 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebas
     id('error').classList.remove("hidden");
     console.log(err);
    }
+
   /**
-   * TODO
    * signIn - Signs the user in based on username and password inputs
    */
   function signIn() {
