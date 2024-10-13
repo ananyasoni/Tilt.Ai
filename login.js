434c6e6764bdd1229743d648c2213e909faa41c6
light611 <dixontan@outlook.com>
2024-10-12 19:37:18 -0700

    finished account page

--- login.js
+++ login.js
@@ -1,28 +1,5 @@
 'use strict';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
-
 (function() {
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCe4FLr_VkD9cmN4Za6VAiGJ5GOtDAuTWQ",
-    authDomain: "tiltai.firebaseapp.com",
-    projectId: "tiltai",
-    storageBucket: "tiltai.appspot.com",
-    messagingSenderId: "818461348700",
-    appId: "1:818461348700:web:335c63db3901d1fff96799",
-    measurementId: "G-371C4M5PC5"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-
   window.addEventListener('load', init);
 
   /**
@@ -38,7 +15,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebas
   async function makeRequest() {
     try {
       let params =  new FormData(id("login_form"));
-      let res = await fetch(BASE_URL, {
+      let res = await fetch('http://127.0.0.1:5500/login.html', {
         method: "POST",
         body: params
       });
@@ -59,7 +36,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebas
    * signIn - Signs the user in based on username and password inputs
    */
   function signIn() {
-    location.assign("index.html");
+    location.assign("account.html");
   }
 
   async function statusCheck(res) {
